1fe44801484ce11dc9b4f00e1c48503e6fb413e5
Nikhil44 <nikhilfunde44@gmail.com>
2023-06-10 12:30:47 +0530

    Added more patterns

--- src/com/StarAndNumberPatterns.java
+++ src/com/StarAndNumberPatterns.java
@@ -8,7 +8,9 @@ public class StarAndNumberPatterns {
 		//pattern.rightTriangle();
 		//pattern.leftTrinagle();
 		//pattern.pyramid();
-		pattern.diamond();
+		//pattern.diamond();
+		//pattern.downwardTriangle();
+		pattern.MirroredRightTriangle();
 	}
 
 //************************************************************************************	
@@ -56,7 +58,7 @@ public class StarAndNumberPatterns {
 	//Diamond shape pattern
 	public void diamond() {
 		
-		int row =8;
+		int row =5;
 		
 		for(int i=1;i<2*row;i++) {
 			for(int k=(row-i);k>0;k--) {
@@ -69,15 +71,45 @@ public class StarAndNumberPatterns {
 			for(int k=1;k<=(i-row);k++) {
 				System.out.print(" ");
 			}
-	// need to check on this logic		
-			for(int j=1;j<=(2*i-1);j++) {
-				System.out.print("*");
-			}
-			
+	// need to check on this logic	
+//			int m=0;
+//			for(int j=1;j<=(row+2-m);j++) {
+//				System.out.print("*");
+//				m--;
+//			}
+//			
 			
 			
 			System.out.println();
 		}
+	}
+	//Downward Triangle
+	public void downwardTriangle() {
+		int row=7;
+		for(int i=0;i<=row;i++) {
+			for(int j=row-i;j>0;j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+	
+	//Mirrored Right Triangle
+	public void MirroredRightTriangle() {
+		
+		int row =5;
+		for(int i=1;i<=row;i++) {
+			for(int k=row-i;k>0;k--) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+		}
+		
+		
 	}
 		
 
